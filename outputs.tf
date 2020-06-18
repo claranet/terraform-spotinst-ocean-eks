9b82c9f78406ecb462ed725b8dd3761330d1aadb
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-06-18 10:49:45 +0100

    Add worker IAM role name output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "worker_iam_role_arn" {
   description = "Default IAM role ARN for EKS worker groups"
   value       = aws_iam_role.workers.arn
 }
+
+output "worker_iam_role_name" {
+  description = "Default IAM role name for EKS worker groups"
+  value       = aws_iam_role.workers.name
+}
