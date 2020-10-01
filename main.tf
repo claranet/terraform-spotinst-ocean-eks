f3271c4723697af4dafcea69e71eabe1be205509
Pawel Fiuto <pawel.fiuto@claranet.uk>
2020-10-01 17:24:18 +0100

    No need to declare providers in the module

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
-provider "spotinst" {
-  token   = var.spotinst_token
-  account = var.spotinst_account
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  load_config_file       = false
-}
-
 resource "random_string" "suffix" {
   length  = 8
   special = false
